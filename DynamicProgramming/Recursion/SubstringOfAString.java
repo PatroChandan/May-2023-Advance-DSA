7984feef648e6e3ed6aea15ebc2b4f9c0e77d2d0
Sagar <sagar@Sagars-MacBook-Air.local>
2023-06-28 22:54:03 +0530

    Substring of a String

---
+++ DynamicProgramming/Recursion/SubstringOfAString.java
@@ -0,0 +1,45 @@
+package DynamicProgramming.Recursion;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class SubstringOfAString {
+    static List<String> resultList = new ArrayList<>();
+    private static void swap(char inputArr[], int index1, int index2) {
+        char temp = inputArr[index1];
+        inputArr[index1] = inputArr[index2];
+        inputArr[index2] = temp;
+    }
+
+    private static void permute(char inputArr[], int left, int right) {
+        if(left == right) {
+            String result = new String(inputArr);
+            resultList.add(result);
+            return;
+        }
+
+        for(int i = left; i <= right; i++) {
+            swap(inputArr, left, i);
+            permute(inputArr, left + 1, right);
+            swap(inputArr, left, i);
+        }
+    }
+
+    public static void main (String[] args) {
+        // Your code here
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        char inputArr[] = input.toCharArray();
+
+        permute(inputArr, 0, inputArr.length - 1);
+
+        Collections.sort(resultList);
+
+        for(String result : resultList) {
+            System.out.print(result + " ");
+        }
+    }
+}
